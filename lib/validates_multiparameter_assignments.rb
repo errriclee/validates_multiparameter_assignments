89e99ba98a8e9e95ab071ad9ba8455ee7ebb0428
Eric <ericworking@gmail.com>
2009-04-02 15:29:53 -0700

    Initial version

---
+++ lib/validates_multiparameter_assignments.rb
@@ -0,0 +1,30 @@
+module ActiveRecord
+  module Validations
+    module ClassMethods
+      def validates_multiparameter_assignments(options = {})
+        configuration = { :message => ActiveRecord::Errors.default_error_messages[:invalid] }.update(options)
+        
+        alias_method :assign_multiparameter_attributes_without_rescuing, :assign_multiparameter_attributes
+        attr_accessor :assignment_error_attrs
+        
+        define_method(:assign_multiparameter_attributes) do |pairs|
+          self.assignment_error_attrs = []
+          begin
+            assign_multiparameter_attributes_without_rescuing(pairs)
+          rescue ActiveRecord::MultiparameterAssignmentErrors
+            $!.errors.each do |error|
+              self.assignment_error_attrs << error.attribute
+            end
+          end
+        end
+        private :assign_multiparameter_attributes
+        
+        validate do |record|
+          record.assignment_error_attrs && record.assignment_error_attrs.each do |attr|
+            record.errors.add(attr, configuration[:message])
+          end
+        end
+      end
+    end
+  end
+end
